d5ce857b33a90e4d822b428753da8c65df5434a0
laerciogermano <laerciogermanoo@gmail.com>
2015-06-11 00:33:34 -0300

    CRUD Básico

--- #NodeJs - 4 - CRUD básico/server.js
+++ #NodeJs - 4 - CRUD básico/server.js
@@ -3,6 +3,11 @@
 // módulos nativo do node
 var http = require('http');
 var querystring = require('querystring');
+var cors = require('cors');
+var request = require('request');
+
+//Models
+var Storage = require('./models/Storage');
 
 // módulos que precisam ser instalados
 var express = require('express');
@@ -15,6 +20,7 @@ var ejs = require('ejs');
 var server = http.createServer(app);
 
 // Pega os dados de requisição do formulário post e transforma em objeto {}
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -33,8 +39,32 @@ app.get('/', function(req, res){
 	res.render('index');
 });
 
+app.post('/api/album/create', function(req, res){
+	var params = req.body;
+	Storage.add(params);
+	res.end('Adicionado com sucesso');
+});
+
+app.get('/api/album/get/:nome', function(req, res){
+	var nome = req.params.nome;
+	var album = Storage.get(nome);
+	res.json(album);
+});
+
+app.get('/api/album/remove/:nome', function(req, res){
+	var nome = req.params.nome;
+	Storage.del(nome);
+	res.end('Removido com sucesso');
+});
+
+app.get('/api/album/getall', function(req, res){
+	var all = Storage.getAll();
+	res.json(all);
+});
+
+
 // Inicia o servidor
-server.listen(5000, '127.0.0.1', function(){
+server.listen(5000, '0.0.0.0', function(){
 	console.log('Servidor iniciado');
 });
 
